df7ad8bc1da8cdb5386d397a0aff85b5c95895b9
Simeon Lam <simeon.j.lam@gmail.com>
2021-09-05 13:52:16 -0700

    root changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "/", to: "homepage#home" as: "root"
+  get "/", to: "homepage#home", as: "root"
   get "/index.html", to: "homepage#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
