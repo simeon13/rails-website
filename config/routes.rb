6fb4f933073ab14ae2a95cebe50e2b1faca4c013
Simeon Lam <simeon.j.lam@gmail.com>
2021-09-06 09:04:29 -0700

    add refresh to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get "/", to: "homepage#home", as: "root"
-  get "/index.html", to: "homepage#home"
+  get "/home", to: "homepage#home", as: "root"
+  get "/", to: "homepage#home"
+  # get "/index.html", to: "homepage#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
